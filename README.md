377e37ec8e3ee35d56185cb121642445199c497c
Samuele Piazzesi <samuelepiazzesi@gmail.com>
2023-04-13 16:05:19 +0200

    fix

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # nextjs-hotjar
 
-
 **Hotjar for Next.js**
 
 This package optimizes script loading using [Next.js `Script` tag](https://nextjs.org/docs/basic-features/script), which means that it will **only work on apps using Next.js >= 11.0.0**.
@@ -8,10 +7,10 @@ This package optimizes script loading using [Next.js `Script` tag](https://nextj
 ## Installation
 
 ```
-pnpm install --save nextjs-hotjar
+pnpm install nextjs-hotjar
 ```
 
-## TL;DR
+## Instructions
 
 Add the `Hotjar` component to your [custom App](https://nextjs.org/docs/advanced-features/custom-app) file:
 
@@ -20,12 +19,12 @@ Add the `Hotjar` component to your [custom App](https://nextjs.org/docs/advanced
 import { Hotjar } from "nextjs-hotjar";
 
 const App = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Hotjar id={YOUR_APP_ID} v={YOUR_APP_VERSION}/>
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<Hotjar id={YOUR_APP_ID} v={YOUR_APP_VERSION} />
+			<Component {...pageProps} />
+		</>
+	);
 };
 
 export default App;
--- src/components/Hotjar.tsx
+++ src/components/Hotjar.tsx
@@ -2,21 +2,19 @@ import React from "react";
 import Script, { ScriptProps } from "next/script";
 
 type HotJarProps = {
-  id: string;
-  sv: number;
-  strategy?: ScriptProps["strategy"];
+	id: string;
+	sv: number;
+	strategy?: ScriptProps["strategy"];
 };
 
 export interface WindowWithHotjar extends Window {
-  hj: (method: string, ...args: any[]) => void;
+	hj: (method: string, ...args: any[]) => void;
 }
 
-declare const window: WindowWithHotjar;
-
 export function Hotjar({ id, sv, strategy = "afterInteractive" }: HotJarProps) {
-  return (
-    <Script strategy={strategy}>
-      {`
+	return (
+		<Script strategy={strategy}>
+			{`
 			<script>
     		(function(h,o,t,j,a,r){
         	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -25,9 +23,9 @@ export function Hotjar({ id, sv, strategy = "afterInteractive" }: HotJarProps) {
         	r=o.createElement('script');r.async=1;
         	r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         	a.appendChild(r);
-    		})(${window},document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 			</script>
 			`}
-    </Script>
-  );
+		</Script>
+	);
 }
