34ae8391694d887ca08e539f1343476639168170
Samuele Piazzesi <samuelepiazzesi@gmail.com>
2023-04-13 16:54:05 +0200

    fix

--- src/components/Hotjar.tsx
+++ src/components/Hotjar.tsx
@@ -16,13 +16,13 @@ export function Hotjar({ id, sv, strategy = "afterInteractive" }: HotJarProps) {
 		<Script strategy={strategy}>
 			{`
     		(function(h,o,t,j,a,r){
-        	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        	h._hjSettings={hjid:${id},hjsv:${sv}}};
-        	a=o.getElementsByTagName('head')[0];
-        	r=o.createElement('script');r.async=1;
-        	r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        	a.appendChild(r);
-    		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:${id},hjsv:${sv}};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 			`}
 		</Script>
 	);
